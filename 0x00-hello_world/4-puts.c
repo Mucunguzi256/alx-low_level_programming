04f481ad310073fa49e1ad582c14b5f9308b9cb8
mucunguzi256 <moemucu@gmail.com>
2023-03-16 16:40:28 -0700

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main program that prints a line with puts function
+ * return: (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle"
+return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * nain - A program that prints the size of various types
+ * return: 0 (success)
+ */
+int main(void)
+{
+char a;
+int i;
+long int b;
+long long int l;
+float f;
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+return (0);
+}
